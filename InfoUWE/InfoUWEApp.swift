c9b2f5cb11b3038576a3974d61a81199b7800edc
Vince Verdadero <chroviver3@gmail.com>
2022-01-30 18:36:28 +0000

    Initial Commit

---
+++ InfoUWE/InfoUWEApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfoUWEApp.swift
+//  InfoUWE
+//
+//  Created by Vince Verdadero on 30/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct InfoUWEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
